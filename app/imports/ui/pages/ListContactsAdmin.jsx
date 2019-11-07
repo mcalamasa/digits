a0033fbd17e9f85d208f99f4dcb56ef490fe4282
mcalamasa <calamasa@hawaii.edu>
2019-11-06 22:46:47 -1000

    contacts collection finished in 37 minutes.

--- app/imports/ui/pages/ListContactsAdmin.jsx
+++ app/imports/ui/pages/ListContactsAdmin.jsx
@@ -3,12 +3,12 @@ import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import Contact from '/imports/ui/components/Contact';
+import ContactAdmin from '/imports/ui/components/ContactAdmin';
 import { Contacts } from '../../api/contact/Contacts';
 
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
-class ListContacts extends React.Component {
+class ListContactsAdmin extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -19,9 +19,9 @@ class ListContacts extends React.Component {
   renderPage() {
     return (
         <Container>
-          <Header as="h2" textAlign="center" inverted>List Contacts</Header>
+          <Header as="h2" textAlign="center" inverted>List Contacts ( Admin )</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) => <Contact key={index} contact={contact}/>)}
+            {this.props.contacts.map((contact, index) => <ContactAdmin key={index} contact={contact}/>)}
           </Card.Group>
         </Container>
     );
@@ -29,7 +29,7 @@ class ListContacts extends React.Component {
 }
 
 /** Require an array of Stuff documents in the props. */
-ListContacts.propTypes = {
+ListContactsAdmin.propTypes = {
   contacts: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
@@ -42,4 +42,4 @@ export default withTracker(() => {
     contacts: Contacts.find({}).fetch(),
     ready: subscription.ready(),
   };
-})(ListContacts);
+})(ListContactsAdmin);
