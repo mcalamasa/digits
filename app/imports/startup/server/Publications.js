c335fc7a1bf76f89004495a7dfac49d50482b460
mcalamasa <calamasa@hawaii.edu>
2019-11-08 20:17:48 -1000

    Update Publications.js

--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -21,7 +21,7 @@ Meteor.publish('StuffAdmin', function publish() {
 });
 
 /** This subscription publishes only the documents associated with the logged in user */
-Meteor.publish('Contact', function publish() {
+Meteor.publish('Contacts', function publish() {
   if (this.userId) {
     const username = Meteor.users.findOne(this.userId).username;
     return Contacts.find({ owner: username });
@@ -30,7 +30,7 @@ Meteor.publish('Contact', function publish() {
 });
 
 /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
-Meteor.publish('ContactAdmin', function publish() {
+Meteor.publish('ContactsAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Contacts.find();
   }
